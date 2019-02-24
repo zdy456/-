f118618b21b6a4e58000ef6b5e2a70064b3005bf
makaixin <1293229934@qq.com>
2019-02-24 17:14:19 +0800

    购物车应用文件整理

--- cms/urls.py
+++ cms/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^', include('users.urls')),
     # url(r'^', include('goods.urls')),
     # url(r'^', include('news.urls')),
-    # url(r'^', include('carts.urls')),
+    url(r'^', include('carts.urls')),
 ]
