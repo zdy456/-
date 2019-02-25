236a3acb76c7b8d726534cf05cc69d177b03e00a
makaixin <1293229934@qq.com>
2019-02-25 18:29:44 +0800

    购物车商品数量接口实现

--- apps/carts/urls.py
+++ apps/carts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from carts import views
+
 urlpatterns = [
-    # url()
+    url(r'^carts/count/$', views.CartsCountView.as_view()),
 ]
 
